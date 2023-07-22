07d9a82c84a0a0f95ad236b8668da541865b7cb8
harshsharma976 <harshsharma4197@gmail.com>
2023-07-22 14:36:49 +0530

    error solve of the code

--- express/express2/Index.js
+++ express/express2/Index.js
@@ -1,17 +1,50 @@
+
+// const express = require('express');
+// const app = express();
+// const fs = require('fs');
+// const port = 8000;
+// const path = require('path')
+// // const sta = path.join(__dirname,"./public");
+
+// app.set('views',path.join(__dirname,'./view'))
+
+// // app.use(express.static (sta));
+// // console.log(sta)
+// app.set('view engine','hbs')
+
+
+// app.get('/',(req,res)=>{
+//     res.render("Profile",{
+//         harsh:"sharma",
+//     })
+  
+// });
+// app.listen(port,()=>{
+//     console.log('Server Created')
+    
+// });
+
+
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const port = 7000;
-const path = require('path')
-const sta = path.join(__dirname,"./public");
+const bodyparse = require('body-parser');
+const port = 8000;
 
-console.log(sta)
+app.use(bodyparse.urlencoded({extended:false}))
+app.use(bodyparse.json())
 
-app.use(express.static (sta));
-console.log(sta)
-
+app.get('/',(req,res)=>{
+    fs.readFile("form.html","utf-8",(err,data)=>{
+        res.end(data);
+    })
+})
+app.post('/Submit',async(req,res)=>{
+    const string =await req.body;
+    console.log(string);
+    res.end("Successfully");
+})
 
 app.listen(port,()=>{
-    // console.log('Server Created')
-    
-});
+    console.log(`your port is listening on ${port}`);
+})
